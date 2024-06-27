baad5a8f4640ed620dbd8f954bef3414f171ac51
Leonardo Sant'Anna <140214338+santannal@users.noreply.github.com>
2024-06-27 12:41:47 -0300

    ajustes de design e responsividade

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-//background
-
-document.addEventListener('mousemove', function (e) {
-    const background = document.querySelector('.background');
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    background.style.backgroundPositionX = mouseX / window.innerWidth * 100 + '%';
-    background.style.backgroundPositionY = mouseY / window.innerHeight * 100 + '%';
-});
-
-
 //instagram
 
 document.getElementById("btninstagram").addEventListener("click", function () {
@@ -20,59 +9,69 @@ document.getElementById("btninstagram").addEventListener("click", function () {
 window.animacao = ScrollReveal({ reset: true });
 
 animacao.reveal('.container.home', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
 animacao.reveal('.navbar', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
 animacao.reveal('.card.prato', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
 animacao.reveal('.title-food', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
 animacao.reveal('.container-fluid.suco', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
 animacao.reveal('.custom-carousel', {
-    duration: 2000,
+    duration: 700,
+    distance: '90px',
+})
+
+animacao.reveal('.container.carousel', {
+    duration: 700,
     distance: '90px',
 })
 
 animacao.reveal('.carousel.slide', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
 
 animacao.reveal('.container-fluid.local-session', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
-animacao.reveal('.local-div', {
-    duration: 2000,
+/*animacao.reveal('.local-div', {
+    duration: 700,
+    distance: '90px',
+})*/
+
+animacao.reveal('.row', {
+    duration: 700,
     distance: '90px',
 })
 
 animacao.reveal('.title-estabelecimento', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
-n
+
 
 animacao.reveal('.contato-session', {
-    duration: 2000,
+    duration: 700,
     distance: '90px',
 })
 
